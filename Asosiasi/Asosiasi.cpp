21ae7bfb7752e1931ec8bc5cb89968070fc9ef6f
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-24 13:20:17 +0700

    method untuk mencetak nama dokter dengan perulangan for

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -36,3 +36,9 @@ public:
 void pasien::tembahDokter(dokter* pDokter) {
 	daftar_dokter.push_back(pDokter);
 }
+void pasien::cetakDokter() {
+	cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+	for (auto& a : daftar_dokter) {
+		cout << a->nama << "\n";
+	}
+}
